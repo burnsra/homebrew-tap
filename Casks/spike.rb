084f9f4e65ec16971512ca379ab34d3a32772146
Robert Burns <burnsra@gmail.com>
2017-01-26 10:51:17 -0500

    Fix Spike cask

--- Casks/spike.rb
+++ Casks/spike.rb
@@ -3,7 +3,7 @@ cask 'spike' do
   sha256 '6aebc81963dfafef927534bd4ef9e55a0261c8657cae0355f16048b299d7e326'
 
   url "https://www.spikeproxy.com/updates/Spike-1.0-89.zip"
-  name 'SquidBar'
+  name 'Spike'
   homepage 'https://www.spikeproxy.com'
   license :mit
 
