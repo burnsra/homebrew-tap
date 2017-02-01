cc09c50fde81c935988dc4a7922fbe692c73ac95
Robert Burns <burnsra@gmail.com>
2017-02-01 08:32:45 -0500

    Addition of Sparkle sign_update

---
+++ Casks/sign-update.rb
@@ -0,0 +1,17 @@
+class SignUpdate < Formula
+  desc "Sparkle - software update framework for macOS (sign_update)"
+  homepage "https://sparkle-project.org/"
+  url "https://github.com/sparkle-project/Sparkle/releases/download/1.16.0/Sparkle-1.16.0.tar.bz2"
+  sha256 "8a928afa342062f433ab9e86fac02bca52ae3ce8352ed0ad2e803a76c19b2d87"
+  head "https://github.com/sparkle-project/Sparkle.git"
+
+  bottle :unneeded
+
+  def install
+    bin.install "bin/sign_update"
+  end
+
+  test do
+    system "#{bin}/sign_update"
+  end
+end
