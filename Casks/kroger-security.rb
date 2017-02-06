84538455ae1f895054d1c32c2625c1628423bdc9
Robert Burns <burnsra@gmail.com>
2017-02-06 10:47:04 -0500

    Update Kroger security screen saver

--- Casks/kroger-security.rb
+++ Casks/kroger-security.rb
@@ -1,6 +1,6 @@
 cask 'kroger-security' do
-  version '1.0.1'
-  sha256 '85863644c8d679de96f66dc9d9af5e2dae494b7979eae12dd5b9214d41558e5b'
+  version '1.0.2'
+  sha256 '719aac4013947bbfdd4660788b0346bf527ec9cf91b81bd3df0f026177831f58'
 
   url "https://www.robertburns.me/dist/KrogerSecurity.zip"
   name 'Kroger Security Screen Saver'
