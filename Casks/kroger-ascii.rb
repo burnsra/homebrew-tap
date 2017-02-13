2b683350d2f0771f77da65f3710a1461fcadf16d
Robert Burns <burnsra@gmail.com>
2017-02-12 20:48:49 -0500

    Update Kroger ascii screen saver

--- Casks/kroger-ascii.rb
+++ Casks/kroger-ascii.rb
@@ -1,6 +1,6 @@
 cask 'kroger-ascii' do
-  version '1.0.2'
-  sha256 'b1935f9f2802a4d7f78d597775b6e393162e8cc3df0ea9d1dad76a4d97d922d9'
+  version '1.0.3'
+  sha256 '41d72fdae7d5575ca2f3233634bf2c5eda00d7484e9b228df8027b9dfec7734a'
 
   url "https://www.robertburns.me/dist/KrogerAscii.zip"
   name 'Kroger Ascii Screen Saver'
