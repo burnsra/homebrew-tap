d505cddd633dc5a60f923e1ac0a1f5e625504cbe
Robert Burns <burnsra@gmail.com>
2017-07-04 22:10:32 -0400

    Addition of Creative screen saver

---
+++ Casks/creative.rb
@@ -0,0 +1,14 @@
+cask 'creative' do
+  version '1.0.0'
+  sha256 'd897272ffff267ed4ee75ad391cc6dc1fcb5a5d95e5af3f645646a6110806a01'
+
+  url "https://www.robertburns.me/dist/Creative.zip"
+  name 'Creative Screen Saver'
+  homepage 'https://www.robertburns.me'
+  license :mit
+
+  auto_updates true
+  accessibility_access false
+
+  screen_saver 'Creative.saver'
+end
