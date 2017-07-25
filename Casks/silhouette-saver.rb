cee1952064f06c5ecbc095602732f8b4f885bd65
Robert Burns <burnsra@gmail.com>
2017-07-24 22:53:34 -0400

    Addition of Silhouette screen saver

---
+++ Casks/silhouette-saver.rb
@@ -0,0 +1,14 @@
+cask 'silhouette-saver' do
+  version '1.0.0'
+  sha256 'f429c3a968510437520fbd4ce90c5e007fa940c16fa2798c5dd48e80d12634ea'
+
+  url "https://www.robertburns.me/dist/silhouette-saver.zip"
+  name 'Silhouette Screen Saver'
+  homepage 'https://www.robertburns.me'
+  license :mit
+
+  auto_updates true
+  accessibility_access false
+
+  screen_saver 'Silhouette.saver'
+end
