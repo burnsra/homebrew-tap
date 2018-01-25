357f5ffa91e807aaa0838093471b09e42c7e5973
Robert Burns <burnsra@gmail.com>
2018-01-24 21:00:38 -0500

    Update google-cloud-platform-saver version

--- Casks/google-cloud-platform-saver.rb
+++ Casks/google-cloud-platform-saver.rb
@@ -1,6 +1,6 @@
 cask 'google-cloud-platform-saver' do
-  version '1.0.1'
-  sha256 '8f4c354af8b9a56a01eb5f162756c9c8e6b313e0fdd1737fc2e34fd53e6ff36a'
+  version '1.0.2'
+  sha256 'e287cc650305bcace25170f35d264a10e29989b0d4739e6dffd1679ed3db1be0'
 
   url "https://www.robertburns.me/dist/google-cloud-platform-saver.zip"
   name 'Google Cloud Platform Screen Saver'
