ce1a88d46587dfd780e408ef737be7ac751f101c
Robert Burns <burnsra@gmail.com>
2018-10-03 09:33:14 -0400

    Addition of yubico-manager

---
+++ Casks/yubico-manager.rb
@@ -0,0 +1,13 @@
+cask 'yubico-manager' do
+  version '1.0.0'
+  sha256 '55ce5d0501a416ae98341997d8760807ac5716f5489122e8abe531877d84a432'
+
+  url "https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-#{version}-mac.pkg"
+  appcast 'https://developers.yubico.com/yubikey-manager-qt/Release_Notes.html'
+  name 'Yubikey Manager'
+  homepage 'https://developers.yubico.com/yubikey-manager-qt/'
+
+  pkg "yubikey-manager-qt-#{version}-mac.pkg"
+
+  uninstall pkgutil: 'com.yubico.ykman'
+end
