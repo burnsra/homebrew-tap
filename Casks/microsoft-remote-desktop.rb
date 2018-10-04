8b55452300a505f52500b2fd1a9901ac0323ca58
Robert Burns <burnsra@gmail.com>
2018-10-04 09:18:42 -0400

    Update microsoft-remote-desktop to 10.2.1-1143

--- Casks/microsoft-remote-desktop.rb
+++ Casks/microsoft-remote-desktop.rb
@@ -1,12 +1,12 @@
 cask 'microsoft-remote-desktop' do
-    version '10.2.0.1117'
-    sha256 '88ad360cd4f9cb161c0cf92432d2b71dbd6a28033c8aeac25f93558e4ee66254'
+    version '10.2.1-1143'
+    sha256 '468ae7dbc29351b413fbe01fd8b01240185a0d78eebf01219aa39c6fae201618'
   
     url "https://go.microsoft.com/fwlink/?linkid=868963"
     name 'Microsoft Remote Desktop'
     homepage 'https://itunes.apple.com/us/app/microsoft-remote-desktop-10/id1295203466?mt=12'
   
-    app 'Microsoft Remote Desktop.app'
+    pkg "Microsoft_Remote_Desktop_#{version.sub(%r{-.*$}, '')}_installer.pkg"
   
     zap trash: [
                  '~/Library/Preferences/com.microsoft.rdc.macos',
