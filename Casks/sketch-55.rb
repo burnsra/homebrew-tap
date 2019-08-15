0f37bcc89e0365d0a137589a42e7c900616d3809
Robert Burns <burnsra@gmail.com>
2019-08-15 08:20:42 -0400

    Addition of Sketch 55.2-78181

---
+++ Casks/sketch-55.rb
@@ -0,0 +1,25 @@
+cask 'sketch-55' do
+    version '55.2-78181'
+    sha256 'cf2a1ee016f63b59898306f57c00afa3323886fe0228b5f0440c0d4538142f1d'
+
+    url "https://download.sketchapp.com/sketch-#{version}.zip"
+    appcast 'https://download.sketchapp.com/sketch-versions.xml'
+    name 'Sketch'
+    homepage 'https://www.sketchapp.com/'
+
+    auto_updates true
+    depends_on macos: '>= :high_sierra'
+
+    app 'Sketch.app'
+
+    zap trash: [
+                 '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl*',
+                 '~/Library/Application Support/com.bohemiancoding.sketch3',
+                 '~/Library/Caches/com.bohemiancoding.sketch3',
+                 '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3',
+                 '~/Library/Logs/com.bohemiancoding.sketch3',
+                 '~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist',
+                 '~/Library/Preferences/com.bohemiancoding.sketch3.plist',
+                 '~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies',
+               ]
+  end
