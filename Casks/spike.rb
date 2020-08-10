25cd4de4c9cee95bd6d057d0016d40d8569dc5a1
Robert Burns <burnsra@gmail.com>
2020-08-10 12:17:39 -0400

    Update spike to 2.0.0-233

--- Casks/spike.rb
+++ Casks/spike.rb
@@ -1,6 +1,6 @@
 cask 'spike' do
-  version '1.2.0-185'
-  sha256 '7ee752ac95a9a01f0fbc26e3469788454abd05f1bc61d963343a8e938f78affe'
+  version '2.0.0-233-release'
+  sha256 '9b669dc52fcfc4d1d91d369e97b3e87940ca21e1da3903b3ab23b2adfe546a32'
 
   url "https://www.spikeproxy.com/updates/Spike-#{version}.zip"
   name 'Spike'
