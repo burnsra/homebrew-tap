84c04110f38b50d44d5b11f359cee2770bc11bf9
Robert Burns <burnsra@gmail.com>
2021-11-13 18:58:25 -0500

    Fix sign-update

--- Formula/sign-update.rb
+++ Formula/sign-update.rb
@@ -1,7 +1,7 @@
 class SignUpdate < Formula
   desc "Sparkle - software update framework for macOS (sign_update)"
   homepage "https://sparkle-project.org/"
-  url "https://github.com/sparkle-project/Sparkle/releases/download/1.27.1/Sparkle-1.27.1.tar.bz2"
+  url "https://github.com/sparkle-project/Sparkle/releases/download/1.27.1/Sparkle-1.27.1.tar.xz"
   sha256 "20d729f8ca4d07911da31fe0296880cd3176b71738deaf2b7f8de2d96cf63f4e"
   head "https://github.com/sparkle-project/Sparkle.git"
 
