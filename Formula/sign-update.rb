83b3f1590e44daaa3fb692b31027fe0b7e47a84e
Robert Burns <burnsra@gmail.com>
2021-11-15 07:14:29 -0500

    Revert sign-update

--- Formula/sign-update.rb
+++ Formula/sign-update.rb
@@ -1,9 +1,11 @@
 class SignUpdate < Formula
   desc "Sparkle - software update framework for macOS (sign_update)"
   homepage "https://sparkle-project.org/"
-  url "https://github.com/sparkle-project/Sparkle/releases/download/1.27.1/Sparkle-1.27.1.tar.xz"
-  sha256 "20d729f8ca4d07911da31fe0296880cd3176b71738deaf2b7f8de2d96cf63f4e"
+  url "https://github.com/sparkle-project/Sparkle/releases/download/1.16.0/Sparkle-1.16.0.tar.bz2"
+  sha256 "8a928afa342062f433ab9e86fac02bca52ae3ce8352ed0ad2e803a76c19b2d87"
   head "https://github.com/sparkle-project/Sparkle.git"
+
+  bottle :unneeded
 
   def install
     bin.install "bin/sign_update"
