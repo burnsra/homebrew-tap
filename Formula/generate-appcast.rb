22035aea42f3b3aa76d6bf0201f537ed6771de4e
Robert Burns <burnsra@gmail.com>
2022-03-14 19:37:43 -0400

    Update Sparkle

--- Formula/generate-appcast.rb
+++ Formula/generate-appcast.rb
@@ -1,15 +1,15 @@
-class SignUpdate < Formula
-  desc "Sparkle - software update framework for macOS (sign_update)"
+class GenerateAppcast < Formula
+  desc "Sparkle - software update framework for macOS (generate_appcast)"
   homepage "https://sparkle-project.org/"
   url "https://github.com/sparkle-project/Sparkle/releases/download/2.1.0/Sparkle-2.1.0.tar.xz"
   sha256 "bf6ac1caa9f8d321d5784859c88da874f28412f37fb327bc21b7b14c5d61ef94"
   head "https://github.com/sparkle-project/Sparkle.git"
 
   def install
-    bin.install "bin/sign_update"
+    bin.install "bin/generate_appcast"
   end
 
   test do
-    system "#{bin}/sign_update"
+    system "#{bin}/generate_appcast"
   end
 end
