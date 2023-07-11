053efdbf52895e222c67f11b3f78c90941cedb8d
Robert Burns <burnsra@gmail.com>
2023-07-11 07:46:56 -0400

    switch appcast to livecheck

--- Casks/source-sans-pro.rb
+++ Casks/source-sans-pro.rb
@@ -3,7 +3,7 @@ cask "source-sans-pro" do
    sha256 "1186d594ecb1336679a1d102bb6ddbe6041f0fd029482e53f02e46aa3ab011cf"
 
    url "https://github.com/adobe-fonts/source-sans-pro/releases/download/#{version}/source-sans-#{version.sub(/\./, "v")}.zip"
-   appcast "https://github.com/adobe-fonts/source-sans-pro/releases.atom"
+   livecheck "https://github.com/adobe-fonts/source-sans-pro/releases.atom"
    name "Source Sans Pro"
    desc "Fonts designed for user interfaces"
    homepage "https://github.com/adobe-fonts/source-sans-pro"
